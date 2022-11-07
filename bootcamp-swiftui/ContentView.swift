1c85660a63fde1585606dcbdedf6aad945495abe
Robert Sandru <sandrurobert@gmail.com>
2022-11-07 12:46:49 +0200

    Initial Commit

---
+++ bootcamp-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  bootcamp-swiftui
+//
+//  Created by Robert Sandru on 07.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bootcamp-swiftui/bootcamp_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcamp_swiftuiApp.swift
+//  bootcamp-swiftui
+//
+//  Created by Robert Sandru on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct bootcamp_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
