9e68d3e993bc16044e421b6e662bceafd0bf4587
Robert Sandru <sandrurobert@gmail.com>
2022-11-07 12:50:57 +0200

    first commit

---
+++ bootcamp-swiftui/0001-ecommerce-flow/EcommerceFlow.swift
@@ -0,0 +1,20 @@
+//
+//  EcommerceFlow.swift
+//  bootcamp-swiftui
+//
+//  Created by Robert Sandru on 07.11.2022.
+//
+
+import SwiftUI
+
+struct EcommerceFlow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EcommerceFlow_Previews: PreviewProvider {
+    static var previews: some View {
+        EcommerceFlow()
+    }
+}
