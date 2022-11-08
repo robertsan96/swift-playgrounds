36ed2b4a46727f9bc2f1e3455261014a2d3dd447
Robert Sandru <sandrurobert@gmail.com>
2022-11-08 12:30:08 +0200

    0001 - TabView

--- apple/swiftui/bootcamp-swiftui/Flows/0001-ecommerce-flow/EcommerceFlow.swift
+++ apple/swiftui/bootcamp-swiftui/Flows/0001-ecommerce-flow/EcommerceFlow.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct EcommerceFlow: View {
+    
+    @State private var selectedTab = EcommerceFlowTabItem.home
+    
     var body: some View {
-        
-        TabView {
-            Text("da")
+        TabView(selection: $selectedTab) {
+            Text("Favorites")
+                .tabItem { tabItemLabel(.favorites) }
+                .tag(EcommerceFlowTabItem.favorites)
+            Text("Home")
+                .tabItem { tabItemLabel(.home) }
+                .tag(EcommerceFlowTabItem.home)
+            Text("Profile")
+                .tabItem { tabItemLabel(.profile) }
+                .tag(EcommerceFlowTabItem.profile)
         }
+        .accentColor(.orange)
+    }
+}
+
+// MARK: Tab Related Extension
+extension EcommerceFlow {
+    
+    private func tabItemLabel(_ tab: EcommerceFlowTabItem) -> some View {
+        Label(tab.title, systemImage: tab.systemImage)
     }
 }
 
@@ -20,5 +39,25 @@ struct EcommerceFlow_Previews: PreviewProvider {
     static var previews: some View {
         EcommerceFlow()
             .preferredColorScheme(.dark)
+    }
+}
+
+fileprivate enum EcommerceFlowTabItem {
+    case home, favorites, profile
+    
+    var title: String {
+        switch self {
+        case .home: return "Home"
+        case .favorites: return "Favorites"
+        case .profile: return "Profile"
+        }
+    }
+    
+    var systemImage: String {
+        switch self {
+        case .home: return "house"
+        case .favorites: return "heart"
+        case .profile: return "person"
+        }
     }
 }
